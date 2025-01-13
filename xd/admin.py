bf4c7ef315707ac63635dff6673140b213d68b75
dms <aijazmahdavi@gmail.com>
2025-01-13 18:01:30 +0500

    Added models to admin

--- xd/admin.py
+++ xd/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Donor)
+class DonorAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'address']
+
+@admin.register(Mustahiq)
+class MustahiqAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'address']
+
+@admin.register(Credit)
+class CreditAdmin(admin.ModelAdmin):
+    list_display = ['donor', 'payment_category', 'amount', 'date']
+
+@admin.register(Debit)
+class DebitAdmin(admin.ModelAdmin):
+    list_display = ['mustahiq', 'source', 'payment_category', 'amount', 'date']
